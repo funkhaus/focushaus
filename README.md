3c9511362d464b7d1fcc316ed52d036cbc812d6c
Drew Baker <drewrb@gmail.com>
2019-09-27 19:18:45 -0700

    Updated to work with Stackhaus theme framework

--- README.md
+++ README.md
@@ -1,72 +1,21 @@
 # focushaus
 
-> Pick image focal points in Wordpress and preview the results.
+Define a focal point for an image. Requires the `<responsive-image>` component that is part of the Stackhaus Nuxt theme framework. [See here for more information](https://github.com/funkhaus/stackhaus).
 
 ## Usage
-1. Head to the "Media" section of your site and click any image.
-1. Click anywhere on the image to place the focal point at that location.
-1. Click the "Show Preview" button (bottom right of the image) to view a resizable preview.
-    * You can hold ⌘ to click through the preview and edit the focal point.
-1. In your PHP:
 
-```
-<div style="background-image: url(<?php echo 'your-image-url'; ?>); background-position: <?php the_custom_background_position(); ?>;"></div>
+1. Instal plugin into WordPress, along with [stackhaus-backend](https://github.com/funkhaus/stackhaus-backend) and follow the instructions from that.
+1. Head to the "Media" section of your Dashboard and click any image.
+1. Click on the small focal point circle, and drag to the focal point. That's it, it saves automatically.
 
-```
+---
 
-That's it!
-
-## API
-
-All functions accepting a `$target` parameter can accept either an attachment ID/object or a post ID/object. Passing a post  will look for a featured image on that post.
-
-### `get_custom_background_position( $target )`
-
-> `target` (int | WP_Post) (optional, default null)
-
-Returns a string representing the image's focal point. Example return value: `25% 75%`. Default `50% 50%`.
-
-### `get_offset_x( $target )`
-
-> `target` (int | WP_Post) (optional, default null)
-
-Returns a number representing an image's focal point X position in percentage points. Default `50` if none set.
-
-### `get_offset_y( $target )`
-
-> `target` (int | WP_Post) (optional, default null)
-
-Returns a number representing an image's focal point Y position in percentage points. Default `50` if none set.
-
-### `the_custom_background_position( $target )`
-
-> `target` (int | WP_Post) (optional, default null)
-
-Echoes the result of `get_custom_background_position( $target )`.
-
-## TODO:
-* Make updating the input fields manually update the crosshair
-
-## Dev Setup
-
-``` bash
-# install dependencies
-npm install && npm install --only=dev
-
-# build and watch dev version
-npm run dev
-
-# build for production with minification
-npm run build
-```
-
---------
-
-__Focushaus__
+**Focushaus**
 
 http://funkhaus.us
 
-Version: 1.0.2
+Version: 2.0
 
-* 1.0.2 - Documented convenience functions, made input fields readonly
-* 1.0.1 - Added `get_offset_x` and `get_offset_y` convenience functions, placed these in main function
+- 2.0 - Refactored to work with Stackhaus theme framework for Nuxt. Do not install this on a traditional Wordpress theme.
+- 1.0.2 - Documented convenience functions, made input fields readonly.
+- 1.0.1 - Added `get_offset_x` and `get_offset_y` convenience functions, placed these in main function.
--- focushaus.php
+++ focushaus.php
@@ -2,111 +2,26 @@
 /*
 Plugin Name: Focushaus
 Description: Give images focal points.
-Version: 1.0.5
+Version: 2.0
 Author: Funkhaus
 Plugin URI:  https://github.com/funkhaus/focushaus
 Author URI:  http://funkhaus.us
 */
 
-    // Enqueue scripts
-    function load_custom_wp_admin_style($hook) {
-      wp_enqueue_media();
-      wp_enqueue_script( 'jquery' );
-      wp_enqueue_script( 'bundle', plugins_url('dist/bundle.js', __FILE__) );
+/*
+ * Enqueue scripts
+ */
+    function focushaus_admin_scripts() {
+        wp_enqueue_script('jquery');
+        wp_enqueue_script('focal-point', plugin_dir_path( __FILE__ ) . 'js/focal-point.js', 'jquery', '2.0b');
     }
-    add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
+    add_action( 'admin_enqueue_scripts', 'focushaus_admin_scripts' );
 
-    // HTML for fields
-    function focushaus_edit_offset_fields( $form_fields, $post ) {
-
-      ob_start();?>
-        <label for="attachments[<?php echo $post->ID; ?>][_custom_offset_x]">
-          <span class="name">X offset (%)</span>
-          <input readonly type="number" class="focushaus-offset x" name="attachments[<?php echo $post->ID; ?>][_custom_offset_x]" value="<?php echo $post->_custom_offset_x; ?>" />
-        </label>
-        <label for="attachments[<?php echo $post->ID; ?>][_custom_offset_y]">
-          <span class="name">Y offset (%)</span>
-          <input readonly type="number" class="focushaus-offset y"  name="attachments[<?php echo $post->ID; ?>][_custom_offset_y]"  value="<?php echo $post->_custom_offset_y; ?>" />
-        </label>
-      <?php
-
-      $html = ob_get_clean();
-
-      $form_fields['focushaus'] = array(
-        'input' => 'html',
-        'label' => __( 'Focal Point' ),
-        'html'  => $html
-      );
-
-      return $form_fields;
+/*
+ * Enqueue styles
+ */
+    function focushaus_admin_styles() {
+        wp_enqueue_style('focal-point', plugin_dir_path( __FILE__ ) . 'css/focal-point.css', null, '2.0b');
     }
-    add_filter( 'attachment_fields_to_edit', 'focushaus_edit_offset_fields', 10, 2 );
-
-    // Update offset meta
-    function focushaus_save_offset( $attachment_id ) {
-      if ( isset( $_REQUEST['attachments'][$attachment_id]['_custom_offset_x'] ) ) {
-          $offset_x = $_REQUEST['attachments'][$attachment_id]['_custom_offset_x'];
-          update_post_meta( $attachment_id, '_custom_offset_x', $offset_x );
-      }
-      if ( isset( $_REQUEST['attachments'][$attachment_id]['_custom_offset_y'] ) ) {
-          $offset_y = $_REQUEST['attachments'][$attachment_id]['_custom_offset_y'];
-          update_post_meta( $attachment_id, '_custom_offset_y', $offset_y );
-      }
-    }
-    add_action( 'edit_attachment', 'focushaus_save_offset' );
-
-    /*
-     * Convenience functions
-     */
-    function get_offset_x($target_post = null){
-      // Get the featured image if we have one
-      if( has_post_thumbnail($target_post) ){
-        $target_post = get_post_thumbnail_id($target_post);
-      }
-
-      $offset_x = get_post_meta($target_post, '_custom_offset_x', true);
-      if( empty($offset_x) ){
-          $offset_x = 50;
-      }
-
-      return $offset_x;
-    }
-
-    function get_offset_y($target_post = null){
-      // Get the featured image if we have one
-      if( has_post_thumbnail($target_post) ){
-        $target_post = get_post_thumbnail_id($target_post);
-      }
-
-      $offset_y = get_post_meta($target_post, '_custom_offset_y', true);
-      if( empty($offset_y) ){
-          $offset_y = 50;
-      }
-
-      return $offset_y;
-    }
-
-    function get_custom_background_position($target_post = null){
-      // Get the offset values
-      $offset_x = get_offset_x($target_post);
-      $offset_y = get_offset_y($target_post);
-
-      return $offset_x . '% ' . $offset_y . '%';
-    }
-
-    function the_custom_background_position($target_post = null){
-      echo get_custom_background_position($target_post);
-    }
-
-    // handle Rest-Easy if it exists
-    function add_focushaus_background_position($input){
-        $input['focus'] = array(
-            'x' => get_offset_x($input['ID']),
-            'y' => get_offset_y($input['ID'])
-        );
-        return $input;
-    }
-    add_filter('rez_serialize_attachment', 'add_focushaus_background_position');
-
-
+    add_action( 'admin_print_styles', 'focushaus_admin_styles' );
 ?>
